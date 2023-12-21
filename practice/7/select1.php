b33070711197e84b993b4f4168eca1f13af5891e
shimojit <0liberty4life@gmail.com>
2023-12-21 11:33:56 +0900

    chapter07完了

--- practice/7/select1.php
+++ practice/7/select1.php
@@ -1,17 +1,24 @@
 <?php
 
-$dsn = 'mysql:dbname=testdb;host=db;charset=utf8;';
+$dsn = 'mysql:dbname=testdb;host=db;charset=utf8';
 $user = 'testuser';
 $password = 'testpass';
 
 try {
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO user (id, name, age, email) VALUES (NULL, '田中三郎', '28', 'sample5@sample5.com')";
+  $sql = "SELECT * FROM user";
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
-  echo '接続に成功しました。';
+  $data = array();
+  $count = $stmt->rowCount();
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $data[] = $row;
+  }
+  echo '処理が終了しました。';
 }catch(PDOException $e) {
   print($e->getMessage());
   die();
 }
+var_dump($count);
+var_dump($data);
