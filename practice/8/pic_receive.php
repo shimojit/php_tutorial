737ee6a7cbc64446a07680cedc730a1352a95885
shimojit <0liberty4life@gmail.com>
2023-12-22 13:15:46 +0900

    テキストのソースコードを一部変更

--- practice/8/pic_receive.php
+++ practice/8/pic_receive.php
@@ -2,8 +2,20 @@
 
 $err = array();
 $img = $_FILES['img'];
+/* exif_imagetype関数を有効にできないため、コメントアウト
 var_dump($img);
+$type = exif_imagetype($img['tmp_name']);
 
+if($type !== IMAGETYPE_JPEG && $type !== IMAGETYPE_PNG) {
+  $err['pic'] = '対象ファイルはJPGまたはPNGのみです。';
+}elseif($img['size'] > 102400) {
+  $err['pic'] = 'ファイルサイズは100KB以下にしてください。';
+}else {
+  $extension = pathinfo($img['name'], PATHINFO_EXTENSION);
+  $new_img = md5(uniqid(mt_rand(), true)).'.'.$extension;
+  move_uploaded_file($img['tmp_name'], './img/'.$img['name']);
+}
+*/
 move_uploaded_file($img['tmp_name'], './img/'.$img['name']);
 ?>
 
